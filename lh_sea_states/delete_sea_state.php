d02341a558650d07eb6bf299db8df4c582c80486
stevencramton <stevencramton@gmail.com>
2025-11-24 10:57:59 -0500

    Update delete_sea_state.php

--- lh_sea_states/delete_sea_state.php
+++ lh_sea_states/delete_sea_state.php
@@ -3,7 +3,6 @@ session_start();
 include '../../mysqli_connect.php';
 include '../../templates/functions.php';
 
-// Security checks
 if (!isset($_SERVER['HTTP_X_REQUESTED_WITH']) || strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) !== 'xmlhttprequest') {
     http_response_code(403);
     die(json_encode(['success' => false, 'message' => 'Invalid request']));
@@ -21,11 +20,11 @@ if ($sea_state_id <= 0) {
     exit();
 }
 
-// Check if there are any signals using this sea state
 $check_query = "SELECT COUNT(*) as count FROM lh_signals WHERE sea_state_id = ?";
 $check_stmt = mysqli_prepare($dbc, $check_query);
 mysqli_stmt_bind_param($check_stmt, 'i', $sea_state_id);
 mysqli_stmt_execute($check_stmt);
+
 $check_result = mysqli_stmt_get_result($check_stmt);
 $check_row = mysqli_fetch_assoc($check_result);
 
@@ -37,12 +36,11 @@ if ($check_row['count'] > 0) {
     mysqli_stmt_close($check_stmt);
     exit();
 }
-
 mysqli_stmt_close($check_stmt);
 
-// Delete the sea state
 $query = "DELETE FROM lh_sea_states WHERE sea_state_id = ?";
 $stmt = mysqli_prepare($dbc, $query);
+
 mysqli_stmt_bind_param($stmt, 'i', $sea_state_id);
 
 if (mysqli_stmt_execute($stmt)) {
@@ -51,11 +49,11 @@ if (mysqli_stmt_execute($stmt)) {
         'message' => 'Sea state deleted successfully'
     ]);
 } else {
+    error_log('Delete sea state error (Sea State ID: ' . $sea_state_id . '): ' . mysqli_error($dbc));
     echo json_encode([
         'success' => false,
-        'message' => 'Failed to delete sea state: ' . mysqli_error($dbc)
+        'message' => 'Failed to delete sea state. Please try again.'
     ]);
 }
-
 mysqli_stmt_close($stmt);
 ?>
